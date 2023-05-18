9606d9f31909fc168a65b28aa90ddc17698df734
Fire-5 <61576440+Fire-5@users.noreply.github.com>
2023-05-18 12:33:14 +0700

    Update and rename timer-to-zero.ino to timer-to-zero_v2.ino by Fire5
    1. Добавил дебаг строки вывода на экран сераилапорта
    2. Подправил логику
    3. Поправил скобочки (была ошибка)
    4. Добавил комментарии по коду

---
+++ timer-to-zero/timer-to-zero_v2.ino
@@ -0,0 +1,145 @@
+#define CLK 2
+  #define DIO 3
+
+  #include <GyverTM1637.h>
+  GyverTM1637 disp(CLK, DIO);
+
+  enum state  {Setup1, ZeroTime, Kaboom, Idle};
+  int current_state = Setup1;
+  int time_on  = 1;
+  int time_off = 1;
+  uint32_t tic = 0;
+  uint32_t tac = 0;
+  uint32_t boom = 9; //время запала
+  uint32_t myTimer = 0;
+  uint32_t timerStop = 0;
+
+
+
+  void setup() {
+    Serial.begin(9600);
+    disp.clear();        // чистим дисплей
+    disp.brightness(7);  // яркость, 0 - 7 (минимум - максимум)
+    pinMode(4, INPUT_PULLUP); // старт
+    pinMode(5, INPUT_PULLUP); // стоп
+    pinMode(6, INPUT_PULLUP); // верх
+    pinMode(7, INPUT_PULLUP); // вниз
+    pinMode(8, INPUT_PULLUP); // триггер
+    analogWrite(255, 0); // реле
+  }
+
+
+  void loop() {
+    // Set Value
+    bool buttonPlus  = false;
+    bool buttonMinus = false;
+    bool buttonStart = false;
+    bool buttonStop  = false;
+    bool timeEvent   = false;
+    bool pushButton  = false;
+    bool trigger     = false;
+
+    bool DEBUG = true;  // Переменная для вывода специальной иформации в serialport
+
+    if (digitalRead(4) == 0 ||
+        digitalRead(5) == 0 ||
+        digitalRead(6) == 0 ||
+        digitalRead(7) == 0   
+       ) {
+      delay(200);  //считывает состояние каждой кнопки каждые 200мс
+                   //создает событие при нажатии на кнопку
+      if (digitalRead(6) == 0) buttonPlus  = true, pushButton = true;
+      if (digitalRead(7) == 0) buttonMinus = true, pushButton = true;
+      if (digitalRead(4) == 0) buttonStart = true, pushButton = true;
+      if (digitalRead(5) == 0) buttonStop  = true, pushButton = true;
+      if (digitalRead(8) == 0) trigger     = true, pushButton = true;
+      
+      if (DEBUG) {
+        String dbgStr = String("Status button | " + buttonPlus  + " | "
+                                                  + buttonMinus + " | "
+                                                  + buttonStart + " | "
+                                                  + buttonStop  + " | "
+                                                  + trigger     + " | ");
+        Serial.println(dbgStr);  // Вывод на экран состояния нажатия кнопок
+      }
+    }
+
+    // Запуск посекундного таймера, для точного отсчета времени
+    if (millis() - myTimer >= 1000) {
+      myTimer = millis();
+      timeEvent = true;
+    }
+    
+    //////////////////////// Состояние настройки ////////////////////////////
+    if (current_state == Setup1) { // кнопками задаем время
+      if      (buttonPlus)  time_on ++;
+      else if (buttonMinus) time_on --;
+      else if (buttonStart) current_state = Idle, tic = 0;
+      
+      if      (time_on < 1) time_on = 1;  // проверка на дурака, что бы нельзя было задать время с отрицательным значением
+      if      (timeEvent || pushButton) {
+        disp.displayInt(time_on);
+        if (DEBUG) {
+          String dbgStr = String("Current state = " + current_state + 
+                                 " | time on  = " + time_on + 
+                                 " | time_off = " + tic );
+          Serial.println(dbgStr);  // Вывод на экран состояния 
+        }
+      }
+    }
+    
+    //////////////////////// Состояние ожидания ////////////////////////////
+    if (current_state == Idle) {
+      if( trigger == true){
+        current_state == ZeroTime;
+        tic = 0;
+      }  
+      if (DEBUG) {
+          String dbgStr = String("Current state = " + current_state + 
+                                 " | time on  = " + time_on + 
+                                 " | time_off = " + tic );
+          Serial.println(dbgStr);  // Вывод на экран состояния 
+        }
+    }
+    
+    //////////////////////// Состояние обратного отсчета ////////////////////////////
+    if (current_state == ZeroTime) {
+      if (trigger == false){
+        current_state == Idle;
+      }
+      
+      if (time_on > tic && timeEvent){    
+        disp.displayInt(time_on - tic), Serial.println(time_on - tic); // добавить минус на дисплей для понимания состояния
+        tic++;
+      } else {
+        current_state == Kaboom;
+      }
+      
+      if (DEBUG) {
+          String dbgStr = String("Current state = " + current_state + 
+                                 " | time on  = " + time_on + 
+                                 " | time_off = " + tic );
+          Serial.println(dbgStr);  // Вывод на экран состояния 
+        }
+    }
+  
+    //////////////////////// Состояние активации ////////////////////////////
+    if (current_state == Kaboom){
+      if (tac > boom && timeEvent){
+        disp.displayInt(8888);
+        analogWrite(0, 0); // Инвертированное реле
+        tac++;
+      } else {
+        disp.displayInt(0000);
+        analogWrite(255,0); // Выключаем реле
+      }
+      
+      if (DEBUG) {
+          String dbgStr = String("Current state = " + current_state + 
+                                 " | boom     = " + time_on + 
+                                 " | tac      = " + tac );
+          Serial.println(dbgStr);  // Вывод на экран состояния 
+      }
+    }
+
+  }
